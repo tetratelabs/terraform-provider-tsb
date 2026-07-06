15dbdba01b6f4c582aab74137b6ab9ab30b2b6d1
Marc Cirauqui <marc@tetrate.io>
2026-07-06 12:35:56 +0200

    chore: remove 2023 scaffold ahead of monorepo migration

--- examples/resources/tsb_service_account/resource.tf
+++
@@ -1,7 +0,0 @@
-resource "tsb_service_account" "example" {
-  name         = "ciautomation"
-  parent       = "organizations/tetrate"
-  display_name = "CI Automation"
-  description  = "Used by our CI tool to create resources"
-  key_encoding = "JWT"
-}
--- examples/resources/tsb_tenant/resource.tf
+++
@@ -1,7 +0,0 @@
-resource "tsb_tenant" "example" {
-  parent          = "organizations/tetrate"
-  name            = "tenantone"
-  display_name    = "Tenant One"
-  description     = "I'm a tenant"
-  security_domain = "production"
-}
